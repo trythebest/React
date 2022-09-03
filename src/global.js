e11fc67d592b577adf11931d7b7b7adc36e03467
trythebest <manikandan.nagarajan.1998@gmail.com>
2022-09-03 14:32:02 +0530

    reactapp deploy

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
 //export const API="https://624e6fb677abd9e37c86ff94.mockapi.io";
-export const API="http://localhost:4000";
+//export const API="http://localhost:4000";
+export const API ="https://mani-node.herokuapp.com";
